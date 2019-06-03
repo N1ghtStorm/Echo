dd33adf5b20df4fe7dafe19f0d6eb4c7a255daa7
N1ght <bushuevdv@mail.ru>
2019-06-03 21:47:50 +0300

    03062019 added Patient Form

--- Echo/Echo/MainForm.Designer.cs
+++ Echo/Echo/MainForm.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.MainFormHeaderPanel = new System.Windows.Forms.Panel();
             this.MainFormCloseButton = new System.Windows.Forms.Button();
+            this.InsideFormPanel = new System.Windows.Forms.Panel();
             this.MainFormHeaderPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,12 +54,21 @@
             this.MainFormCloseButton.UseVisualStyleBackColor = true;
             this.MainFormCloseButton.Click += new System.EventHandler(this.MainFormCloseButton_Click);
             // 
+            // InsideFormPanel
+            // 
+            this.InsideFormPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.InsideFormPanel.Location = new System.Drawing.Point(2, 30);
+            this.InsideFormPanel.Name = "InsideFormPanel";
+            this.InsideFormPanel.Size = new System.Drawing.Size(922, 522);
+            this.InsideFormPanel.TabIndex = 1;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(103)))), ((int)(((byte)(104)))));
             this.ClientSize = new System.Drawing.Size(927, 554);
+            this.Controls.Add(this.InsideFormPanel);
             this.Controls.Add(this.MainFormHeaderPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
@@ -73,5 +83,6 @@
 
         private System.Windows.Forms.Panel MainFormHeaderPanel;
         private System.Windows.Forms.Button MainFormCloseButton;
+        private System.Windows.Forms.Panel InsideFormPanel;
     }
 }
---
+++ Echo/Echo/PatientCardForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Echo
+{
+    partial class PatientCardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.BackColor = System.Drawing.Color.White;
+            this.NameTextBox.Location = new System.Drawing.Point(81, 46);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.NameTextBox.Size = new System.Drawing.Size(110, 20);
+            this.NameTextBox.TabIndex = 0;
+            this.NameTextBox.TextChanged += new System.EventHandler(this.NameTextBox_TextChanged);
+            // 
+            // PatientCardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(103)))), ((int)(((byte)(104)))));
+            this.ClientSize = new System.Drawing.Size(740, 482);
+            this.Controls.Add(this.NameTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PatientCardForm";
+            this.Text = "PatientCardForm";
+            this.Load += new System.EventHandler(this.PatientCardForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameTextBox;
+    }
+}
